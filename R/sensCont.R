4a2adc51f6ad056e4cb53002d16e613250b10445
Ngendahimana <dkn18@case.edu>
2016-11-06 00:32:42 -0500

    first commit

---
+++ R/sensCont.R
@@ -0,0 +1,112 @@
+#' Sensitivity Analysis for A Simple Comparison for continious data
+#'
+#' This function allows you to assess how sensitive your results are to unmeasured variable.
+
+#' @param data A matched sample
+#' @param exp A variables defining exposure group
+#' @param outcome The outcome variable
+#' @param failtime Time to event
+#' @param Gamma Bias to be assessed
+#' @param Gammainterval interval between two hidden bias to be assessed
+#' @param alpha Significance level
+#' @keywords Sensitivity
+#' @references Section 4.4.8. of Rosenbaum PR (2002) Observational Studies, 2nd Edition.
+#' @export
+#' @examples  data("Telemedicine.matchedsample") # Reading in the data produced by a matching method
+#'
+#' sensCont(data=Telemedicine.matchedsample,exp="Telehealth.n",outcome = "GFRoneyr",Gamma=1.5,Gammainterval = 0.01)
+
+
+
+sensCont = function(data,exp,outcome,Gamma,Gammainterval){
+
+  results = list()
+  data1c =subset(data,select =c("matches",exp,outcome))
+  names(data1c)[c(2:3)] = c("exp","outcome")
+  data2c= subset(data1c,exp == 0)
+  data3c = subset(data1c,exp == 1)
+  data4c = subset(data2c,select =c("matches","exp","outcome"))
+  data5c = subset(data3c,select =c("matches","exp","outcome"))
+  data6c= dplyr::full_join(data4c,data5c,by="matches")
+
+  names(data6c)[c(3,5)] = c("noexpOutcome","expOutcome")
+
+  pairs = nrow(data6c)
+  results$pairs = nrow(data6c)
+  data6c$absdiff =  abs(data6c$expOutcome-data6c$noexpOutcome)
+
+  for(i in 1:nrow(data6c)){
+    if ( data6c[i,5]>data6c[i,3]) {
+      data6c$sign[i] = 1
+
+    } else if (data6c[i,5] <data6c[i,3]) {
+      data6c$sign[i] = -1
+    } else
+      data6c$sign[i] = 0
+
+  }
+
+  data7c=data6c[with(data6c, order(absdiff)), ]
+
+  for(i in 1:nrow(data7c)){
+    data7c$Rank[i]=i
+  }
+
+  g <- data7c$absdiff
+  l <- split(data7c,data7c$absdiff)
+  l <- lapply(l, transform, mRank = mean(Rank))
+  data7c <- unsplit(l, g)
+
+  data8c = subset(data7c, sign== -1 )
+  teststat=sum(data8c$mRank)
+
+  results$Test.statistic = sum(data8c$mRank)
+
+
+
+  gamVal=seq(1,Gamma,by=Gammainterval)
+  pplus = 1/(1+gamVal)
+  pminus = gamVal/(1+gamVal)
+
+  table1=data.frame(cbind(gamVal,pplus,pminus))
+  table1$expTplus=pairs*table1$pplus
+  table1$expTminus = table1$pminus*(pairs*(pairs+1)/2)
+  table1$sd_expT = sqrt(table1$pplus*(1-table1$pplus)*(pairs*(pairs+1)*(2*pairs+1)/6))
+
+
+  for(i in 1:length(gamVal)){
+    table1$pupper[i]=round(min(1,2*pnorm((teststat-table1[i,5])/table1[i,6],lower.tail = FALSE) ),4)
+  }
+
+  for(i in 1:length(gamVal)){
+    table1$plower[i]=round(min(1,2*pnorm((teststat-table1[i,4])/table1[i,6],lower.tail = FALSE) ),4)
+  }
+
+  results$sensitivitytable = table1
+
+  table1$min = abs(0.05-table1$pupper)
+
+  vrt = table1[table1$min==min(table1$min),]$gamVal
+  hrz = table1[table1$min==min(table1$min),]$pupper
+
+  plot(table1$pupper~table1$gamVal,type="l",xlab="Gamma",ylab="p-val upper bound",main="Sensitivity analysis plot")
+  segments(x0=0,y0=hrz,x1=vrt,y1=hrz,col = "pink",lty = "dashed",lwd = 3)
+  segments(x0=vrt,y0=0,x1=vrt,y1=hrz,col = "pink",lty = "dashed",lwd = 3)
+
+  #text(1.05, 0.25, paste("Gamma",vrt),cex = 1)
+  #text(1.05, 0.24, paste("p-value",hrz),cex = 1)
+
+  results$upperbound_pval =table1[table1$min==min(table1$min),]$pupper
+  results$Gamma = table1[table1$min==min(table1$min),]$gamVal
+
+  return(results)
+
+}
+
+
+
+
+
+
+
+
---
+++ data-raw/cleaningdata.R
@@ -0,0 +1,86 @@
+library(rms); library(dplyr); library(ggplot2); library(survival);  library(foreign); library(haven); library(reshape); library(mosaic); library(Matching); library(Epi); library(tableone); library(faraway); library(Hmisc); library(pander);library(dplyr);library(tableone);library(Epi);library(survival);library(Matching);library(sm);library(optmatch);library(RItools);library(nnet);library(Hmisc);library(lattice);library(twang);library(survival);library(knitr);library(MatchIt)
+
+
+
+
+if (!file.exists("data-raw/Telemedicine2.csv")) {
+  Telemedicine <- read_csv("~/Dropbox/Dissertation_David Ngendahimana/package_development/SensR/data-raw/Telemedicine2.csv")
+}
+
+
+#### ESDR
+Telemedicine$ESRD.HTN = as.numeric(Telemedicine$ESRD == "HTN")
+Telemedicine$ESRD.Other = as.numeric(Telemedicine$ESRD == "Other")
+Telemedicine$ESRD.PKD = as.numeric(Telemedicine$ESRD == "PKD")
+Telemedicine$ESRD.diab = as.numeric(Telemedicine$ESRD == "Diabetes")
+Telemedicine$ESRD.glm = as.numeric(Telemedicine$ESRD == "Glomerulonephritis")
+
+#### Induction
+Telemedicine$Induction = ifelse(Telemedicine$Induction=="THYMO","THYMO","SIMULECT")
+Telemedicine$Induction.SIM = as.numeric(Telemedicine$Induction == "SIMULECT")
+Telemedicine$Induction.THY = as.numeric(Telemedicine$Induction == "THYMO")
+
+###PSrisk
+
+Telemedicine$PSrisk = as.factor(ifelse(Telemedicine$PSrisk == "LOW","LOW","MODERATE"))
+Telemedicine$PSrisk.Low = as.numeric(Telemedicine$PSrisk == "LOW")
+Telemedicine$PS.Moderate = as.numeric(Telemedicine$PSrisk == "MODERATE")
+
+####Race
+Telemedicine$Race1 = as.factor(ifelse(Telemedicine$Race == "WHITE","WHITE","OTHER"))
+Telemedicine$Race.wht = as.numeric(Telemedicine$Race1 == "WHITE")
+Telemedicine$Race.other = as.numeric(Telemedicine$Race1 == "OTHER")
+
+
+### Hypertension
+Telemedicine$HypertensionYES = as.numeric(Telemedicine$Hypertension == "YES")
+Telemedicine$HypertensionNO = as.numeric(Telemedicine$Hypertension == "YES")
+
+
+Telemedicine$Readmission = factor(Telemedicine$Readmission,level = c("YES","NO"))
+
+
+Telemedicine$Surgeon.eqs <- as.numeric(Telemedicine$Surgeon=="eqs")
+Telemedicine$Surgeon.kjw <- as.numeric(Telemedicine$Surgeon=="kjw")
+Telemedicine$Surgeon.vrh <- as.numeric(Telemedicine$Surgeon=="vrh")
+
+Telemedicine$GenderM = as.numeric(Telemedicine$Gender=="MALE")
+Telemedicine$GenderF = as.numeric(Telemedicine$Gender=="FEMALE")
+
+Telemedicine$HeartdzYES  = as.numeric(Telemedicine$Heartdz =="YES")
+Telemedicine$HeartdzNO  = as.numeric(Telemedicine$Heartdz =="NO")
+
+Telemedicine$TransplantTypeLiving  = as.numeric(Telemedicine$TransplantType=="Living")
+Telemedicine$TransplantTypeDeceased  = as.numeric(Telemedicine$TransplantType=="Deceased")
+
+Telemedicine$DGFYES  = as.numeric(Telemedicine$DGF=="YES")
+Telemedicine$DGFNO  = as.numeric(Telemedicine$DGF=="NO")
+
+Telemedicine$Telehealth.n=as.numeric(Telemedicine$Telehealth)
+
+
+
+Telemedicine$Graftstatus = ifelse(Telemedicine$Graftstatus=="FAIL","FAIL","FUNCTION")
+
+Telemedicine$Telehealth = factor(Telemedicine$Telehealth,level = c("YES","NO"))
+Telemedicine$Telehealth.n =  abs(as.numeric(Telemedicine$Telehealth)-2)
+
+
+psmodel1 <- glm(Telehealth.n ~ Age + Gender + Race1 + ESRD + BMI  + TransplantType + Hypertension + Dialysis + DGF + Induction + PSrisk + Surgeon, family=binomial(), data=Telemedicine)## removed all improper variables.-> ps from model makes sense i.e Patients on Telehealth have lower propensity for readmission.
+
+Telemedicine$ps <- psmodel1$fitted
+Telemedicine$linps <- psmodel1$linear.predictors
+Telemedicine=subset(Telemedicine,ps >0.001)
+
+set.seed(23547)
+X <- Telemedicine$linps
+Tr <- as.logical(abs(as.numeric(Telemedicine$Telehealth)-2))
+match1 <- Match(Tr=Tr, X=X, M = 1, replace=FALSE, ties=FALSE)
+
+#summary(match1)
+
+matches <- factor(rep(match1$index.treated, 2))
+Telemedicine.matchedsample <- cbind(matches, Telemedicine[c(match1$index.control, match1$index.treated),])
+
+save(Telemedicine.matchedsample, file = "data/Telemedicine.matchedsample.csv")
+
