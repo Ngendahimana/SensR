7dc38bcf1274933514016dff5e33052451cad535
Ngendahimana <dkn18@case.edu>
2016-11-09 20:33:38 -0500

    added line to open pdf file from R. Package biobase require to install this function

--- R/edaTable1.R
+++ R/edaTable1.R
@@ -24,7 +24,9 @@ res2 <- compareGroups::compareGroups(formula, data = matched, ref = 1)
 table01=compareGroups::createTable(res1,show.p.mul = TRUE,show.p.overall = TRUE)
 table02=compareGroups::createTable(res2, show.p.mul =TRUE,show.p.overall = TRUE)
 xptable01=cbind("Before matching"= table01,"After matching"=table02)
-export2pdf(xptable01,'table1.pdf',size="small")
+compareGroups::export2pdf(xptable01,'table1.pdf',size="small")
+Biobase::openPDF(normalizePath("table1.pdf"))
+
 #results$CombTable = xptable01
 #return(results)
 
